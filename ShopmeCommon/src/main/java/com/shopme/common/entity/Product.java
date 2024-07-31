f7a6ea1adbbd6515bd537710e0040513438fb23a
antran9128 <an482194@gmail.com>
2024-07-31 23:01:52 +0700

    code product entity and repository

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -0,0 +1,78 @@
+package com.shopme.common.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(unique = true, length = 255, nullable = false)
+	private String name;
+
+	@Column(unique = true, length = 255, nullable = false)
+	private String alias;
+
+	@Column(length = 512, nullable = false, name = "short_description")
+	private String shortDescription;
+
+	@Column(length = 4096, nullable = false, name = "full_description")
+	private String fullDescription;
+
+	@Column(name = "created_time", nullable = false, updatable = false)
+	private Date createdTime;
+
+	@Column(name = "updated_time")
+	private Date updatedTime;
+
+	private boolean enabled;
+
+	@Column(name = "in_stock")
+	private boolean inStock;
+
+	// Generate automatically column name as "cost"
+	private float cost;
+
+	// Generate automatically column name as "price"
+	private float price;
+
+	@Column(name = "discount_percent")
+	private float discountPercent;
+	
+	private float length;
+	private float width;
+	private float height;
+	private float weight;
+	
+//	private int reviewCount;
+//	private float averageRating;
+//	
+//	@Column(name = "main_image", nullable = false)
+//	private String mainImage;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToOne
+	@JoinColumn(name = "brand_id")	
+	private Brand brand;
+}
